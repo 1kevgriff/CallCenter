bcf6b094f56b9293c10749d4c684ca3b3f51e451
Kevin Griffin <kevin@kevgriffin.com>
2012-05-30 15:43:52 -0400

    initial

---
+++ Source/CallCenter/CallCenter.Web/Controllers/PhoneController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallCenter.Web.Controllers
+{
+    public class PhoneController : Controller
+    {
+        //
+        // GET: /Phone/
+
+        public ActionResult IncomingCall()  
+        {
+            return View();
+        }
+
+    }
+}
