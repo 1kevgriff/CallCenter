75346b3f1e7265415c71568d69099b68eba123ca
Kevin Griffin <kevin@kevgriffin.com>
2012-05-30 16:30:30 -0400

    adding servicerequest

--- Source/CallCenter/CallCenter.Web/Controllers/PhoneController.cs
+++ Source/CallCenter/CallCenter.Web/Controllers/PhoneController.cs
@@ -17,21 +17,43 @@ namespace CallCenter.Web.Controllers
 
         public ActionResult IncomingCall(string CallSid)
         {
-            string accountSid = "";
-            string authToken = "";
-
-            TwilioRestClient client = new TwilioRestClient(accountSid, authToken);
-            var call = client.GetCall(CallSid);
+            var call = GetClient(CallSid);
 
             TwilioResponse response = new TwilioResponse();
             response.Say("Welcome to the Bank of Griff.");
-            response.Say("Goodbye");
-            response.Hangup();
+            response.Pause();
+            response.Say("Press 1 to manage your account.");
+            response.Say("Press 2 to take out a loan.");
+            response.Say("Press 3 to talk to a representative.");
+            response.Gather(new {action = Url.Action("ServiceRequest"), timeout = 10, method = "POST", finishOnKey = "", numDigits = 1});
 
             Stream result = new MemoryStream(Encoding.Default.GetBytes(response.ToString()));
 
             return new FileStreamResult(result, "text/plain");
         }
 
+        private static Call GetClient(string CallSid)
+        {
+            string accountSid = "";
+            string authToken = "";
+
+            TwilioRestClient client = new TwilioRestClient(accountSid, authToken);
+            var call = client.GetCall(CallSid);
+            return call;
+        }
+
+        public ActionResult ServiceRequest(string CallSid, string Digits)
+        {
+            var call = GetClient(CallSid);
+            TwilioResponse response = new TwilioResponse();
+            response.Say(string.Format("You pressed {0}", Digits));
+            response.Pause();
+            response.Say("Way to go.");
+            response.Hangup();
+
+            Stream result = new MemoryStream(Encoding.Default.GetBytes(response.ToString()));
+
+            return new FileStreamResult(result, "text/plain");
+        }
     }
 }
