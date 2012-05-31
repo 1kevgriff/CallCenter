a2f8386899790d412727f19cf93c80957b744d09
Kevin Griffin <kevin@kevgriffin.com>
2012-05-31 12:03:34 -0400

    adding signalr

---
+++ Source/CallCenter/CallCenter.Web/Hubs/DashboardHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallCenter.Web.Hubs
+{
+    public class DashboardHub : SignalR.Hubs.Hub
+    {
+        public void PreloadData()
+        {
+            
+        }
+    }
+}
---
+++ Source/CallCenter/CallCenter.Web/StateManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CallCenter.Web.Hubs;
+using SignalR;
+using Twilio;
+
+namespace CallCenter.Web
+{
+    public class StateManager
+    {
+        private List<Call> ActiveCalls { get; set; }
+        private List<Call> InactiveCalls { get; set; }
+
+        private DashboardHub _context = (DashboardHub) GlobalHost.ConnectionManager.GetHubContext<DashboardHub>();
+
+        public StateManager()
+        {
+            ActiveCalls = new List<Call>();
+            InactiveCalls = new List<Call>();
+        }
+        
+        public void AddNewCall(Call call)
+        {
+            ActiveCalls.Add(call);
+            BroadcastUpdatedCalls();
+        }
+
+        public void CompletedCall(Call call)
+        {
+            ActiveCalls.Remove(call);
+            InactiveCalls.Add(call);
+        }
+
+        private void BroadcastUpdatedCalls()
+        {
+            _context.Clients.updateActiveCalls(ActiveCalls);
+            _context.Clients.updateInactiveCalls(InactiveCalls);
+        }
+    }
+}
